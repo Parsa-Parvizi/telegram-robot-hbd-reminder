0001975cdb05317698cf81434bc47e9a86e98a52
Parsa Parvizi <138492415+Parsa-Parvizi@users.noreply.github.com>
2025-07-17 00:09:05 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام! ربات یادآور تولد فعاله 🎉")
 
-app = ApplicationBuilder().token("توکن ربات تلگرامت").build()
+app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
